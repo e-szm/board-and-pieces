9122539a5d46ca45dc29aa2d258d50b0c26d254a
Eddie Szmigielski <edward.szmi@gmail.com>
2024-03-29 18:38:18 -0500

    Add static method for parsing and appending ECO to a match document from the PGN.

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -1,7 +1,8 @@
 const Match = require("../models/matchModel");
 
 exports.createMatch = async (req, res, next) => {
-  const newMatch = Match.create(req.body);
+  Match.appendECO(req.body);
+  console.log(req.body);
 
   res.status(200).json({
     status: "success",
--- models/matchModel.js
+++ models/matchModel.js
@@ -7,6 +7,25 @@ const matchSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  url: String,
+  end_time: Number,
+  rated: Boolean,
+  rules: {
+    type: String,
+    enum: [
+      "chess",
+      "chess960",
+      "bughouse",
+      "kingofthehill",
+      "threecheck",
+      "crazyhouse",
+    ],
+  },
+  time_class: {
+    type: String,
+    enum: ["daily", "rapid", "blitz", "bullet"],
+  },
+  time_control: String,
   white: {
     rating: Number,
     result: {
@@ -32,6 +51,39 @@ const matchSchema = new mongoose.Schema({
     username: String,
     uuid: UUID,
   },
+  black: {
+    rating: Number,
+    result: {
+      type: String,
+      enum: [
+        "win",
+        "checkmated",
+        "agreed",
+        "repetition",
+        "timeout",
+        "resigned",
+        "stalemate",
+        "lose",
+        "insufficient",
+        "50move",
+        "abandoned",
+        "kingofthehill",
+        "threecheck",
+        "timevsinsufficient",
+        "bughousepartnerlos",
+      ],
+    },
+    username: String,
+    uuid: UUID,
+  },
 });
+
+matchSchema.statics.appendECO = function (matches) {
+  for (let match of matches) {
+    const re = /ECO \"[A-E][0-9][0-9]\"/;
+    const ecoStart = match.pgn.search(re) + 5;
+    match.eco = match.pgn.slice(ecoStart, ecoStart + 3);
+  }
+};
 
 module.exports = mongoose.model("Match", matchSchema);
