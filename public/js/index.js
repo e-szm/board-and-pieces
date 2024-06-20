4e2bd7a655c75343109a1c2f3a43719d7d3134e5
Eddie Szmigielski <edward.szmi@gmail.com>
2024-06-20 12:23:26 -0500

    Add dynamic mobile navigation and refactor navigation click handlers.

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,21 @@
 
 import logout from "./logout.js";
 
+function handleClickNav(e) {
+  const clicked = e.target;
+
+  if (clicked.id === "logout") logout();
+  else if (clicked.classList.contains("mobile-nav-btn")) {
+    const action = clicked.dataset.action;
+    this.classList.remove("open", "closed");
+    this.classList.add(action);
+  }
+}
+
 function init() {
-  const logoutBtn = document.getElementById("logout");
-  if (logoutBtn) logoutBtn.addEventListener("click", logout);
+  const navListContainer = document.querySelector(".nav-list-container");
+  if (navListContainer)
+    navListContainer.addEventListener("click", handleClickNav);
 }
 
 init();
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,9 +1,7 @@
 import { logout } from "./utils/api.js";
 import alert from "./utils/alert.js";
 
-export default async function (e) {
-  e.preventDefault();
-
+export default async function () {
   const res = await logout();
   if (!res.ok || res.status !== 200) {
     alert("error", "Oops! There was an issue. Please try again later.");
