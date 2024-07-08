02beabba9bad1dfb40093d36a7e93b46f2f8f416
Eddie Szmigielski <edward.szmi@gmail.com>
2024-07-08 09:46:18 -0500

    Update commented out https redirect

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ const app = express();
 
 // All Heroku and enforce https
 app.enable("trust proxy");
-// if (process.env.NODE_ENV === "production") {
-//   app.use((req, res, next) => {
-//     if (req.header("x-forwarded-proto") !== "https") {
-//       res.redirect(`https://${req.header("host")}${req.originalUrl}`);
-//     } else next();
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header("host")}${req.originalUrl}`);
+    } else next();
+  });
+}
 
 // Setup pug templates
 app.set("view engine", "pug");
